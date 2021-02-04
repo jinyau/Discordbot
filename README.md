0a3b5aaf598328f0c53af300fe8e1f30ad1b3c71
Jin Qiu <jin.qiu@temple.edu>
2021-02-03 23:44:58 -0500

    Revert "Revert "Added simple voice channel commands""
    This reverts commit fedd511e070f57ec1e02188f4aa8904950c61647.

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
  Basic Discord Bot
 
  Primary purpose is to delete any mention of the word 'boruto'.
+ 
  #todo
+    
     - Music integration
+    
     - hosting on AWS
+    
     - API integration (league, stocks, etc)
+    
     - basic ml algorithm 
 
 
--- bot.py
+++ bot.py
@@ -3,11 +3,16 @@ import discord
 import random
 import re
 import unidecode
+import youtube_dl
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from youtube_dl import YoutubeDL
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
 #GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
@@ -15,6 +20,16 @@ intents = discord.Intents.default()
 bot = commands.Bot(command_prefix = '!', intents=intents)
 
 banned_words = ['boruto', 'ボルト', 'ぼると', 'b0ruto', 'b0rut0', 'borut0', 'ボuルzaトmaki', 'oturob', 'bouzarumatoki', '8oruto', '80ruto', '8orut0', '80rut0']
+
+YDL_OPTIONS = {
+        'format': 'bestaudio',
+        'noplaylist':'True'
+    }
+
+FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn'
+    }
 
 
 @bot.event
@@ -30,7 +45,7 @@ async def on_message(message):
     get_message = parse_message(message)
 
     if any(x in get_message.lower() for x in banned_words):
-        channel = bot.get_channel(804193490241978420)
+        channel = bot.get_channel(CHANNEL_ID)
         await message.delete() 
         response = 'Deleted from ' + message.author.name + ' in ' + message.channel.name + ':\n' + message.content
         await channel.send(content=response)
@@ -49,6 +64,39 @@ async def fk_aundre(ctx):
     aundre_winter = 'I am Aundre Winter and my opinions are the worst in the world.'
     response = aundre_winter
     await ctx.send(response)
+
+
+@bot.command(name='play')
+async def play(ctx):
+    voice_channel = ctx.author.voice.channel
+
+
+@bot.command(name='leave')
+async def leave(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='pause')
+async def pause(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send('Audio is already paused.')
+
+
+@bot.command(name='resume')
+async def resume(ctx)
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send('Audio is not paused.')
+
 
 
 def parse_message(message):
